7590b24fc5b14c72b819481d7a4d56f4a279fe0e
Suhassinh017 <suhassinh17@gmail.com>
2026-03-12 13:09:29 +0530

    feat: initialize vitereact project with Vite and React setup
    - Added package.json with dependencies and scripts for development, build, linting, and preview.
    - Included Vite logo SVG in the public directory.
    - Created App.css for styling the application.
    - Implemented App.jsx as the main application component with a simple "Hello World" message.
    - Added React logo SVG in the assets directory.
    - Defined global styles in index.css for consistent theming and layout.
    - Set up main.jsx to render the App component using React's StrictMode.
    - Configured Vite with vite.config.js to use the React plugin.

---
+++ counter/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [counter, setCounter] = useState(0)
+
+
+  // let counter = 5;
+
+  const addvalue = () => {
+    if(counter < 20) {
+      counter = counter + 1;
+    }
+    setCounter(counter)
+  }
+
+  const removevalue = () => {
+    if(counter > 0) {
+      counter = counter - 1
+    }
+    setCounter(counter)
+  }
+
+  return (
+    <>
+    <h1>Hello world</h1>
+    <h2>Counter value: {counter}</h2>
+    <button onClick={addvalue}>Add Value</button>
+    <br/>
+    <button onClick={removevalue}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
